e201ab66f0bfab06480c6f7579b2e6da97d53d4d
Vuong Tran <tqvuongit@gmail.com>
2018-07-22 13:51:24 +0700

    connect store react-redux

--- src/components/HelloWorld/Header.js
+++ src/components/HelloWorld/Header.js
@@ -3,19 +3,19 @@ import PropTypes from 'prop-types';
 import logo from '../../assets/logo.svg';
 
 class Header extends Component {
-	constructor(props) {
-		super(props);
+	// constructor(props) {
+	// 	super(props);
 
-		this.state = {
-			tech: props.tech,
-		};
-	}
+	// 	// this.state = {
+	// 	// 	tech: props.tech,
+	// 	// };
+	// }
 
 	render() {
 		return (
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<h1 className="App-title">Hello {this.state.tech}</h1>
+				<h1 className="App-title"> Hello {this.props.tech}</h1>
 			</header>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import HelloWorldApp from './HelloWorldApp';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -8,7 +10,13 @@ import './assets/common.css';
 
 const helloWorldAppElement = document.getElementById('helloWorldApp');
 
-const render = () => ReactDOM.render(<HelloWorldApp />, helloWorldAppElement);
+const render = () =>
+	ReactDOM.render(
+		<Provider store={store}>
+			<HelloWorldApp />
+		</Provider>,
+		helloWorldAppElement
+	);
 render();
 
 registerServiceWorker();
